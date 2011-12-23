1158a5c97f856982fc3d726076cba5b60f25a34e
Sergey Rura <kseptorl@gmail.com>
2011-12-23 12:54:53 +0200

    12:55

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,3 +9,4 @@ class ApplicationController < ActionController::Base
 end
     
 
+# ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtzF8V5c5w/0v9PD21q39dYP8JaHesy4ujOKs0ka/Pyx3Xigj6T3CBr4jkMHpS8xTSGVaWqiVushVq/AmJaOhmt84RCQjIlWWR0Iaj2kiYcUoFlloempfWN8rnmNwhl3mqlHRzl759iOHZueSF0g4UL2H0DcPwrsXFwP7z8puR4VVV+6swLmwu4krEngTE1e+W6qo0nq7WtnIePytM31U9RiPRTGlPlL0OpzTpaL/ke6UcqPDh7cmS/5vBqVbNap9q3CEr3ItarPDRAmCq4o3DqORj8JGpRD+CupPigpt4XbWSosGCgQqnmcp5FmZzv75/ndRyrabeyWmW/6cHRVonw==  user@key-for-app-19580-first_app
